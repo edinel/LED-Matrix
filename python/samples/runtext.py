bb2d5f5c60f7417d3cb6299ec3ed5b06fa2bf05e
Eddie <edinel@users.noreply.github.com>
2023-09-14 12:02:01 -0700

    I think this will workf

--- python/samples/runtext.py
+++ python/samples/runtext.py
@@ -12,6 +12,7 @@ class RunText(SampleBase):
         self.parser.add_argument("-t", "--text", help="The text to scroll on the RGB LED panel", default="Hello world!")
 
 
+
     def run(self):
         offscreen_canvas = self.matrix.CreateFrameCanvas()
         font = graphics.Font()
