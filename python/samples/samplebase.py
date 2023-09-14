af7ca14bd1df75b3b34b4d3d0d8d420d87561573
Eddie <edinel@users.noreply.github.com>
2023-09-14 16:11:33 -0700

    oops deleted too much before

--- python/samples/samplebase.py
+++ python/samples/samplebase.py
@@ -10,6 +10,7 @@ from rgbmatrix import RGBMatrix, RGBMatrixOptions
 class SampleBase(object):
     def __init__(self, *args, **kwargs):
         
+        self.parser = argparse.ArgumentParser()
 
         self.parser.add_argument("-r", "--led-rows", action="store", help="Display rows. 16 for 16x32, 32 for 32x32. Default: 64", default=64, type=int)
         self.parser.add_argument("--led-cols", action="store", help="Panel columns. Typically 32 or 64. (Default: 64)", default=64, type=int)
@@ -29,6 +30,7 @@ class SampleBase(object):
         self.parser.add_argument("--led-multiplexing", action="store", help="Multiplexing type: 0=direct; 1=strip; 2=checker; 3=spiral; 4=ZStripe; 5=ZnMirrorZStripe; 6=coreman; 7=Kaler2Scan; 8=ZStripeUneven... (Default: 0)", default=0, type=int)
         self.parser.add_argument("--led-panel-type", action="store", help="Needed to initialize special panels. Supported: 'FM6126A'", default="", type=str)
         self.parser.add_argument("--led-no-drop-privs", dest="drop_privileges", help="Don't drop privileges from 'root' after initializing the hardware.", action='store_false')
+        self.parser.add_argument("--font", action="store", help="Choose a font", default="7x13" type=str)
         self.parser.set_defaults(drop_privileges=True)
 
 
@@ -38,12 +40,12 @@ class SampleBase(object):
     def run(self):
         print("Running")
 
-#This next bit is basically "Main" fior all intents and purposes it seems to me.
     def process(self):
         self.args = self.parser.parse_args()
-        
-        print (self.args.font)
+        #vars (self.args)
+
         options = RGBMatrixOptions()
+        print (self.args.font)
 
         if self.args.led_gpio_mapping != None:
           options.hardware_mapping = self.args.led_gpio_mapping
