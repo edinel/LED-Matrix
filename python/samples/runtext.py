00bd5907135eb65741d194dda5d022e75b81efa8
Eddie <edinel@users.noreply.github.com>
2023-09-18 13:02:34 -0700

    trying offset

--- python/samples/runtext.py
+++ python/samples/runtext.py
@@ -28,7 +28,7 @@ class RunText(SampleBase):
 
         while True:
             offscreen_canvas.Clear()
-            len = graphics.DrawText(offscreen_canvas, font, pos, 40, textColor, my_text)
+            len = graphics.DrawText(offscreen_canvas, font, pos, 40, textColor, my_text, 5)
             pos -= 1
             if (pos + len < 0):
                 pos = offscreen_canvas.width
